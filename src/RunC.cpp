92cb45f690a61695bfaf786899c882f8b269a89b
phqb <phqb@protonmail.com>
2026-04-09 09:55:35 +0700

    add C interface

---
+++ src/RunC.cpp
@@ -0,0 +1,76 @@
+// This file is part of fTetWild, a software for generating tetrahedral meshes.
+//
+// Copyright (C) 2019 Yixin Hu <yixin.hu@nyu.edu>
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include "RunC.h"
+#include "Run.h"
+
+#include <array>
+#include <vector>
+
+struct FTW_TetMesh {
+    floatTetWild::TetMesh mesh;
+};
+
+extern "C" {
+
+FTW_TetMesh* ftw_run(const double* in_vertices,  int num_vertices,
+                     const int*    in_triangles, int num_triangles,
+                     double lr, unsigned int max_threads,
+                     int skip_simplify, int log_level)
+{
+    std::vector<std::array<double, 3>> verts(num_vertices);
+    for (int i = 0; i < num_vertices; ++i)
+        verts[i] = {in_vertices[3*i], in_vertices[3*i+1], in_vertices[3*i+2]};
+
+    std::vector<std::array<int, 3>> tris(num_triangles);
+    for (int i = 0; i < num_triangles; ++i)
+        tris[i] = {in_triangles[3*i], in_triangles[3*i+1], in_triangles[3*i+2]};
+
+    auto* result = new FTW_TetMesh;
+    result->mesh = floatTetWild::run(verts, tris, lr, max_threads,
+                                     skip_simplify != 0, log_level);
+    return result;
+}
+
+int ftw_tet_mesh_get_num_vertices(const FTW_TetMesh* mesh)
+{
+    return static_cast<int>(mesh->mesh.vertices.size());
+}
+
+int ftw_tet_mesh_get_num_tetrahedra(const FTW_TetMesh* mesh)
+{
+    return static_cast<int>(mesh->mesh.tetrahedra.size());
+}
+
+void ftw_tet_mesh_get_vertices(const FTW_TetMesh* mesh, double* out)
+{
+    const auto& verts = mesh->mesh.vertices;
+    for (int i = 0; i < (int)verts.size(); ++i) {
+        out[3*i]   = verts[i].x;
+        out[3*i+1] = verts[i].y;
+        out[3*i+2] = verts[i].z;
+    }
+}
+
+void ftw_tet_mesh_get_tetrahedra(const FTW_TetMesh* mesh, int* out)
+{
+    const auto& tets = mesh->mesh.tetrahedra;
+    for (int i = 0; i < (int)tets.size(); ++i) {
+        out[4*i]   = tets[i].v[0];
+        out[4*i+1] = tets[i].v[1];
+        out[4*i+2] = tets[i].v[2];
+        out[4*i+3] = tets[i].v[3];
+    }
+}
+
+void ftw_tet_mesh_free(FTW_TetMesh* mesh)
+{
+    delete mesh;
+}
+
+} // extern "C"
---
+++ src/RunC.h
@@ -0,0 +1,48 @@
+// This file is part of fTetWild, a software for generating tetrahedral meshes.
+//
+// Copyright (C) 2019 Yixin Hu <yixin.hu@nyu.edu>
+// This Source Code Form is subject to the terms of the Mozilla Public License
+// v. 2.0. If a copy of the MPL was not distributed with this file, You can
+// obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#pragma once
+
+// C interface for floatTetWild::run.
+// Usable from C, Python (ctypes/cffi), Rust (FFI), etc.
+//
+// Workflow:
+//   FTW_TetMesh* m = ftw_run(verts, nv, tris, nt, 0.05, 0, 0, 3);
+//   // query ftw_get_num_vertices / ftw_get_num_tetrahedra, then fill buffers
+//   ftw_free(m);
+//
+// All arrays are flat / row-major:
+//   in_vertices    – doubles [x0,y0,z0, x1,y1,z1, …], length = 3 * num_vertices
+//   in_triangles   – ints    [v0,v1,v2, …],            length = 3 * num_triangles
+//   out_vertices   – doubles [x0,y0,z0, …],            length = 3 * ftw_get_num_vertices(m)
+//   out_tetrahedra – ints    [a,b,c,d, …],             length = 4 * ftw_get_num_tetrahedra(m)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct FTW_TetMesh FTW_TetMesh;
+
+// Run tetrahedralization.  Returns NULL on failure.
+FTW_TetMesh* ftw_run(const double* in_vertices,  int num_vertices,
+                     const int*    in_triangles, int num_triangles,
+                     double lr, unsigned int max_threads,
+                     int skip_simplify, int log_level);
+
+int  ftw_tet_mesh_get_num_vertices(const FTW_TetMesh* mesh);
+int  ftw_tet_mesh_get_num_tetrahedra(const FTW_TetMesh* mesh);
+
+// Copy results into caller-supplied buffers.
+void ftw_tet_mesh_get_vertices(const FTW_TetMesh* mesh, double* out_vertices);
+void ftw_tet_mesh_get_tetrahedra(const FTW_TetMesh* mesh, int* out_tetrahedra);
+
+void ftw_tet_mesh_free(FTW_TetMesh* mesh);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
